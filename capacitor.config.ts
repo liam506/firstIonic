e9df71e977be17371bc4dd21ed84e20566df5845
William Njau <willymboche369@gmail.com>
2024-12-18 19:00:41 -0800

    update cap

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'firstionic.com',
+  appName: 'ionic-app-base',
+  webDir: 'build'
+};
+
+export default config;
